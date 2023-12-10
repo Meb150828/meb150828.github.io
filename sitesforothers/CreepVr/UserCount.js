e6f2c9e397695ed89dcc7e9865ba711e7d0c4863
Meb150828 <114597287+Meb150828@users.noreply.github.com>
2023-12-09 18:45:50 -0600

    Added Creep Vr Webpage

---
+++ sitesforothers/CreepVr/UserCount.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch data from the API
+    fetch('https://discord.com/api/guilds/1138218821472231514/widget.json')
+        .then(response => response.json())
+        .then(data => {
+            // Check if the "members" property exists in the data
+            if (data.members) {
+                // Get the number of members in the list
+                const memberCount = data.members.length;
+
+                // Update the member count in the HTML
+                const memberCountElement = document.getElementById('memberCount');
+                memberCountElement.textContent = `🟢 Members Online: ${memberCount}`;
+            } else {
+                console.error('No "members" property found in the API response.');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});
