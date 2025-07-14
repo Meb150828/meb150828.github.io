5bc9a60c213403308e65193d1f658b43dcf4198e
Meb150828 <114597287+Meb150828@users.noreply.github.com>
2025-07-13 19:17:14 -0500

    Added full game functionality

--- games/catchphrase/js/main.js
+++ games/catchphrase/js/main.js
@@ -18,6 +18,27 @@ async function getRandomWord(AlreadyPlayedWords) {
     return RandomWord;
 }
 
+var timeUp = false
+var timerStarted = false
+async function startTimer(progressBar) {
+    if (timerStarted) {
+        return
+    }
+    timerStarted = true
+    timeUp = false
+    const maxTime = 10
+    var time = 0
+
+    while(time < maxTime) {
+        time += 1
+        let barPercentage = (time / maxTime) * 100
+        progressBar.style.width = `${barPercentage}%`
+        await sleep(1000)
+    }
+    timeUp = true
+    timerStarted = false
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     var gameData = {
         ["Started"]: false,
@@ -32,20 +53,49 @@ document.addEventListener("DOMContentLoaded", () => {
     const skip = document.getElementById("skip-btn")
     const correct = document.getElementById("correct-btn")
 
-    var hasClicked = false
-    playButton.addEventListener("click", async () => {
-        home.style.display = "none"
-        game.style.display = "block"
-        gameData["Started"] = true
+    const progressBar = document.getElementById("progress-bar")
 
+    const loseContainer = document.getElementById("lose-container")
+    const loseContainerDarken = document.getElementById("lose-container-darken")
+
+    var hasClicked = false
+    var correctClicked = false
+
+    async function runGame() {
+        correct.innerText = "Start"
         while(true) {
+            if (hasClicked && correctClicked == true) {
+                console.log("Yes")
+                hasClicked = false
+                gameData["Started"] = true
+                correct.innerText = "Correct"
+                startTimer(progressBar)
+                break
+            }
+            await sleep(50)
+        }
+        while(true) {
+            if (timeUp) {
+                runGame()
+                loseContainer.style.display = "block"
+                loseContainerDarken.style.display = "block"
+                break
+            }
             let randomWord = await getRandomWord(gameData["AlreadyPlayedWords"])
             if (randomWord) {
                 gameWord.innerText = randomWord
             }
             console.log(hasClicked)
             while(hasClicked == false) {
+                if (timeUp) {
+                    break
+                }
                 if (hasClicked) {
+                    if (correctClicked) {
+                        gameData["AlreadyPlayedWords"][randomWord]
+                        console.log(gameData["AlreadyPlayedWords"])
+                        correctClicked = false
+                    }
                     hasClicked = false
                     break
                 }
@@ -54,13 +104,39 @@ document.addEventListener("DOMContentLoaded", () => {
             hasClicked = false
             await sleep(50)
         }
+    }
+
+    playButton.addEventListener("click", async () => {
+        home.style.display = "none"
+        game.style.display = "block"
+
+        runGame()
     })
 
-    skip.addEventListener("click", () => {
+    var skipDebounce = false
+    skip.addEventListener("touchstart", async () => {
+        if (skipDebounce) {
+            return
+        }
+        skipDebounce = true
+        skip.style.pointerEvents = "none"
         hasClicked = true
-    })
+        await sleep(750)
+        skipDebounce = false
+        skip.style.pointerEvents = "auto"
+    }, {passive: true})
 
-    correct.addEventListener("click", () => {
+    var correctDebounce = false
+    correct.addEventListener("touchstart", async () => {
+        if (correctDebounce) {
+            return
+        }
+        correctDebounce = true
+        correct.style.pointerEvents = "none"
+        correctClicked = true
         hasClicked = true
-    })
+        await sleep(2000)
+        correctDebounce = false
+        correct.style.pointerEvents = "auto"
+    }, {passive: true})
 })
